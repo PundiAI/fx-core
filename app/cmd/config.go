ec1bcfd2fd32c22ce8c1cf2d6e66c4b079cb9cb1
zakir <nash@wokoworks.com>
2021-11-10 17:19:21 +0800

    fix config comand

--- app/cmd/config.go
+++ app/cmd/config.go
@@ -21,40 +21,52 @@ func ConfigCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			if args[0] != "app.toml" && args[0] != "config.toml" {
-				return errors.New("invalid config file")
+				return errors.New("invalid config file(support: app.toml,config.toml)")
 			}
 			if len(args) == 2 {
-				fmt.Println(serverCtx.Viper.Get(args[1]))
-				return nil
-			}
-			if len(args) <= 1 {
-				data, err := json.MarshalIndent(serverCtx.Viper.AllSettings(), "", "\t")
+				data, err := json.MarshalIndent(serverCtx.Viper.Get(args[1]), "", "\t")
 				if err != nil {
 					return err
 				}
 				fmt.Println(string(data))
 				return nil
 			}
-			serverCtx.Viper.Set(args[1], args[2])
-
+			if len(args) == 3 {
+				serverCtx.Viper.Set(args[1], args[2])
+			}
 			rootDir := serverCtx.Viper.GetString(flags.FlagHome)
 			configPath := filepath.Join(rootDir, "config")
-			tmCfgFile := filepath.Join(configPath, "config.toml")
-			appCfgFile := filepath.Join(configPath, "app.toml")
-
 			switch args[0] {
 			case "app.toml":
 				var appConfig = config.Config{}
 				if err := serverCtx.Viper.Unmarshal(&appConfig); err != nil {
 					return err
 				}
-				config.WriteConfigFile(appCfgFile, &appConfig)
+				if len(args) == 1 {
+					data, err := json.MarshalIndent(appConfig, "", "  ")
+					if err != nil {
+						return err
+					}
+					fmt.Println(string(data))
+					return nil
+				}
+				configPath = filepath.Join(configPath, "app.toml")
+				config.WriteConfigFile(configPath, &appConfig)
 			case "config.toml":
 				var tmConfig = tmcfg.Config{}
 				if err := serverCtx.Viper.Unmarshal(&tmConfig); err != nil {
 					return err
 				}
-				tmcfg.WriteConfigFile(tmCfgFile, &tmConfig)
+				if len(args) == 1 {
+					data, err := json.MarshalIndent(tmConfig, "", "  ")
+					if err != nil {
+						return err
+					}
+					fmt.Println(string(data))
+					return nil
+				}
+				configPath := filepath.Join(configPath, "config.toml")
+				tmcfg.WriteConfigFile(configPath, &tmConfig)
 			}
 
 			fmt.Printf("update configuration file: %s\n", configPath)
