160b85c46098796a726e4a5b34827520aa1a81d1
zakir <nash@wokoworks.com>
2021-11-16 11:10:39 +0800

    feat: update tendermint key add second confirmation

--- app/cmd/update.go
+++ app/cmd/update.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/client/input"
 	"os"
 	"path/filepath"
 
@@ -16,20 +19,42 @@ import (
 	"github.com/functionx/fx-core/app"
 )
 
+const (
+	flagUpdateNodeKey = "update-node-key"
+	flagUpdatePrivKey = "update-priv-key"
+	flagUnsafe        = "unsafe"
+)
+
 func UpdateValidatorKeyCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-validator [mnemonic]",
-		Short: "update validator node private key json",
+		Use:   "update-validator [secret]",
+		Short: "update validator node consensus private key file (.fxcore/config/priv_validator_key.json)",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			rootDir := serverCtx.Viper.GetString(flags.FlagHome)
 			if len(rootDir) <= 0 {
-				return fmt.Errorf("home dir can't be empty")
+				return errors.New("please set the home directory")
 			}
-			valMnemonic := args[0]
+			unsafe := serverCtx.Viper.GetBool(flagUnsafe)
+			updateKey := serverCtx.Viper.GetBool(flagUpdatePrivKey)
+			if (unsafe && !updateKey) || (!unsafe && updateKey) {
+				return fmt.Errorf("the flags %s and %s must be used together", flagUnsafe, flagUpdatePrivKey)
+			}
+			if !unsafe && !updateKey {
+				buf := bufio.NewReader(cmd.InOrStdin())
+				if yes, err := input.GetConfirmation("WARNING: The consensus private key of the node will be replaced. Ensure that the backup is complete. USE AT YOUR OWN RISK. Continue?", buf, cmd.ErrOrStderr()); err != nil {
+					return err
+				} else if !yes {
+					return nil
+				}
+			}
+
+			secret := args[0]
 			pvKeyFile := serverCtx.Config.PrivValidatorKeyFile()
-			_ = os.Remove(pvKeyFile)
+			if err := os.Remove(pvKeyFile); err != nil {
+				return err
+			}
 			if err := tmos.EnsureDir(filepath.Dir(pvKeyFile), 0777); err != nil {
 				return err
 			}
@@ -37,37 +62,54 @@ func UpdateValidatorKeyCmd() *cobra.Command {
 			if err := tmos.EnsureDir(filepath.Dir(pvStateFile), 0777); err != nil {
 				return err
 			}
-			valPrivKey := privval.NewFilePV(ed25519.GenPrivKeyFromSecret([]byte(valMnemonic)), pvKeyFile, pvStateFile)
+			valPrivKey := privval.NewFilePV(ed25519.GenPrivKeyFromSecret([]byte(secret)), pvKeyFile, pvStateFile)
 			valPrivKey.Save()
 			return nil
 		},
 	}
+	cmd.Flags().Bool(flagUpdatePrivKey, false, "Update ed25519 private key. Requires --unsafe.")
+	cmd.Flags().Bool(flagUnsafe, false, "Enable unsafe operations. This flag must be switched on along with all unsafe operation-specific options.")
 	return cmd
 }
 
 func UpdateNodeKeyCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-node-key",
-		Short: "update node key",
+		Use:   "update-node-key [secret]",
+		Short: "update node key file (fxcore/config/node_key.json)",
 		Args:  cobra.RangeArgs(0, 1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			rootDir := serverCtx.Viper.GetString(flags.FlagHome)
 			if len(rootDir) <= 0 {
-				return fmt.Errorf("home dir can't be empty")
+				return errors.New("please set the home directory")
 			}
-			consMnemonic := app.NewMnemonic()
+			unsafe := serverCtx.Viper.GetBool(flagUnsafe)
+			updateKey := serverCtx.Viper.GetBool(flagUpdateNodeKey)
+			if (unsafe && !updateKey) || (!unsafe && updateKey) {
+				return fmt.Errorf("the flags %s and %s must be used together", flagUnsafe, flagUpdateNodeKey)
+			}
+			if !unsafe && !updateKey {
+				buf := bufio.NewReader(cmd.InOrStdin())
+				if yes, err := input.GetConfirmation("WARNING: The node key of the node will be replaced. Ensure that the backup is complete. USE AT YOUR OWN RISK. Continue?", buf, cmd.ErrOrStderr()); err != nil {
+					return err
+				} else if !yes {
+					return nil
+				}
+			}
+			secret := app.NewMnemonic()
 			if len(args) > 0 {
-				consMnemonic = args[0]
+				secret = args[0]
 			}
 			nodeKeyFile := serverCtx.Config.NodeKeyFile()
 			if err := tmos.EnsureDir(filepath.Dir(nodeKeyFile), 0777); err != nil {
 				return err
 			}
 
-			nodeKey := &p2p.NodeKey{PrivKey: ed25519.GenPrivKeyFromSecret([]byte(consMnemonic))}
+			nodeKey := &p2p.NodeKey{PrivKey: ed25519.GenPrivKeyFromSecret([]byte(secret))}
 			return nodeKey.SaveAs(nodeKeyFile)
 		},
 	}
+	cmd.Flags().Bool(flagUpdateNodeKey, false, "Update node key. Requires --unsafe.")
+	cmd.Flags().Bool(flagUnsafe, false, "Enable unsafe operations. This flag must be switched on along with all unsafe operation-specific options.")
 	return cmd
 }
