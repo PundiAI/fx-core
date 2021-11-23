b80be0860c54635959dff10a58ededa6e348d05e
devon <devon@wokoworks.com>
2021-11-23 11:07:32 +0800

    update: fxcored debug addr support with address prefix

--- app/cmd/debug.go
+++ app/cmd/debug.go
@@ -9,6 +9,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/functionx/fx-core/app/fxcore"
+	"github.com/spf13/viper"
 	"github.com/tendermint/tendermint/libs/bytes"
 	"strconv"
 	"strings"
@@ -214,38 +216,49 @@ func PubkeyCmd() *cobra.Command {
 }
 
 func AddrCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "addr [address]",
 		Short: "Convert an address between hex and bech32",
 		Long:  "Convert an address between hex encoding and bech32.",
 		Example: fmt.Sprintf(
-			`$ %s debug addr ethm10jmp6sgh4cc6zt3e8gw05wavvejgr5pw2unfju
+			`$ %s debug addr fx1walm8hnaylycy5s45uc9n3cc50mwhhhcep9ksl
 $ %s debug addr 0xA588C66983a81e800Db4dF74564F09f91c026351`, version.AppName, version.AppName),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			addrString := args[0]
-			cfg := sdk.GetConfig()
-
+			prefix := viper.GetString("prefix")
+			valPrefix := prefix + sdk.PrefixValidator + sdk.PrefixOperator
 			var addr []byte
 			switch {
 			case common.IsHexAddress(addrString):
 				addr = common.HexToAddress(addrString).Bytes()
-			case strings.HasPrefix(addrString, cfg.GetBech32ValidatorAddrPrefix()):
-				addr, _ = sdk.ValAddressFromBech32(addrString)
-			case strings.HasPrefix(addrString, cfg.GetBech32AccountAddrPrefix()):
-				addr, _ = sdk.AccAddressFromBech32(addrString)
+			case strings.HasPrefix(addrString, prefix), strings.HasPrefix(addrString, valPrefix):
+				_, bz, err := bech32.DecodeAndConvert(addrString)
+				if err != nil {
+					return fmt.Errorf("decode fxcore address error %v", err)
+				}
+				addr = bz
 			default:
-				return fmt.Errorf("expected a valid hex or bech32 address (acc prefix %s), got '%s'", cfg.GetBech32AccountAddrPrefix(), addrString)
+				return fmt.Errorf("expected a valid hex or bech32 address (acc prefix %s), got '%s'", prefix, addrString)
 			}
-
+			acc, err := bech32.ConvertAndEncode(prefix, addr)
+			if err != nil {
+				return fmt.Errorf("convert and encode address error %v", err)
+			}
+			val, err := bech32.ConvertAndEncode(valPrefix, addr)
+			if err != nil {
+				return fmt.Errorf("convert and encode val address error %v", err)
+			}
 			cmd.Println("Address bytes:", addr)
 			cmd.Printf("Address (hex): %s\n", bytes.HexBytes(addr))
 			cmd.Printf("Address (EIP-55): %s\n", common.BytesToAddress(addr))
-			cmd.Printf("Bech32 Acc: %s\n", sdk.AccAddress(addr))
-			cmd.Printf("Bech32 Val: %s\n", sdk.ValAddress(addr))
+			cmd.Printf("Bech32 Acc: %s\n", acc)
+			cmd.Printf("Bech32 Val: %s\n", val)
 			return nil
 		},
 	}
+	cmd.Flags().String("prefix", fxcore.AddressPrefix, "fxcore address prefix")
+	return cmd
 }
 
 func RawBytesCmd() *cobra.Command {
