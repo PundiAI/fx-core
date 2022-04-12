d0bac7111f4a285c866017092fc96d0952021bf1
fx0x55 <dennis@wokoworks.com>
2022-04-12 11:04:03 +0800

    fix: add Gravity TransferAfter receive address verify.

--- x/gravity/keeper/ibc_hook.go
+++ x/gravity/keeper/ibc_hook.go
@@ -1,6 +1,10 @@
 package keeper
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	fxtypes "github.com/functionx/fx-core/types"
+	"github.com/functionx/fx-core/x/gravity/types"
+)
 
 // TransferAfter Hook operation after transfer transaction triggered by IBC module
 func (k Keeper) TransferAfter(ctx sdk.Context, sender, receive string, amount, fee sdk.Coin) error {
@@ -8,6 +12,13 @@ func (k Keeper) TransferAfter(ctx sdk.Context, sender, receive string, amount, f
 	sendAddr, err := sdk.AccAddressFromBech32(sender)
 	if err != nil {
 		return err
+	}
+
+	// verify receive address 2022-04-12
+	if ctx.BlockHeight() >= fxtypes.EvmSupportBlock() {
+		if err = types.ValidateEthAddressAndValidateChecksum(receive); err != nil {
+			return err
+		}
 	}
 	_, err = k.AddToOutgoingPool(ctx, sendAddr, receive, amount, fee)
 	return err
