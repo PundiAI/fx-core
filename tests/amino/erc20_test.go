c875610d5c4fdfaa6bb01f5ce3a849d01737c531
zakir <nash@wokoworks.com>
2022-10-13 11:15:25 +0800

    test: amino encode RegisterCoinProposal

--- tests/amino/erc20_test.go
+++ tests/amino/erc20_test.go
@@ -10,18 +10,38 @@ import (
 	erc20types "github.com/functionx/fx-core/v2/x/erc20/types"
 )
 
-func TestAminoEncodeUpdateDenomAliasProposal(t *testing.T) {
+func TestAminoEncodeRegisterCoinProposal(t *testing.T) {
 	encode := app.MakeEncodingConfig()
 	proposal := erc20types.RegisterCoinProposal{
 		Title:       "v2",
 		Description: "foo",
-		Metadata:    types.Metadata{},
+		Metadata: types.Metadata{
+			Description: "test",
+			DenomUnits: []*types.DenomUnit{
+				{
+					Denom:    "test",
+					Exponent: 0,
+					Aliases: []string{
+						"ethtest",
+					},
+				},
+				{
+					Denom:    "TEST",
+					Exponent: 18,
+					Aliases:  []string{},
+				},
+			},
+			Base:    "test",
+			Display: "test",
+			Name:    "test name",
+			Symbol:  "TEST",
+		},
 	}
 	data, err := encode.Amino.MarshalJSON(proposal)
 	require.NoError(t, err)
-	require.Equal(t, `{"title":"v2","description":"foo","metadata":{}}`, string(data))
+	require.Equal(t, `{"title":"v2","description":"foo","metadata":{"description":"test","denom_units":[{"denom":"test","aliases":["ethtest"]},{"denom":"TEST","exponent":18}],"base":"test","display":"test","name":"test name","symbol":"TEST"}}`, string(data))
 
 	marshal, err := encode.Marshaler.MarshalJSON(&proposal)
 	require.NoError(t, err)
-	require.Equal(t, `{"title":"v2","description":"foo","metadata":{"description":"","denom_units":[],"base":"","display":"","name":"","symbol":""}}`, string(marshal))
+	require.Equal(t, `{"title":"v2","description":"foo","metadata":{"description":"test","denom_units":[{"denom":"test","exponent":0,"aliases":["ethtest"]},{"denom":"TEST","exponent":18,"aliases":[]}],"base":"test","display":"test","name":"test name","symbol":"TEST"}}`, string(marshal))
 }
