93eaa67c094a056785fd1aeba2bb458118f07f3f
zakir <nash@wokoworks.com>
2022-12-26 20:32:17 +0800

    refactor: signature evidence

--- x/crosschain/keeper/evidence.go
+++ x/crosschain/keeper/evidence.go
@@ -1,26 +1,30 @@
 package keeper
 
 import (
-	"bytes"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/functionx/fx-core/v3/x/crosschain/types"
 )
 
-// SetPastExternalSignatureCheckpoint puts the checkpoint of a oracle set, batch, or logic call into a set
+// SetPastExternalSignatureCheckpoint puts the checkpoint of a oracle set, batch into a set
 // in order to prove later that it existed at one point.
 func (k Keeper) SetPastExternalSignatureCheckpoint(ctx sdk.Context, checkpoint []byte) {
 	store := ctx.KVStore(k.storeKey)
-	store.Set(types.GetPastExternalSignatureCheckpointKey(checkpoint), []byte{0x1})
+	store.Set(types.GetPastExternalSignatureCheckpointKey(uint64(ctx.BlockHeight()), checkpoint), []byte{0x1})
 }
 
-// GetPastExternalSignatureCheckpoint tells you whether a given checkpoint has ever existed
-func (k Keeper) GetPastExternalSignatureCheckpoint(ctx sdk.Context, checkpoint []byte) (found bool) {
+// IteratePastExternalSignatureCheckpoint iterates through all PastExternalSignatureCheckpoint in the half-open interval [start,end)
+func (k Keeper) IteratePastExternalSignatureCheckpoint(ctx sdk.Context, start uint64, end uint64, cb func([]byte) bool) {
 	store := ctx.KVStore(k.storeKey)
-	if bytes.Equal(store.Get(types.GetPastExternalSignatureCheckpointKey(checkpoint)), []byte{0x1}) {
-		return true
-	} else {
-		return false
+	startKey := append(types.PastExternalSignatureCheckpointKey, sdk.Uint64ToBigEndian(start)...)
+	endKey := append(types.PastExternalSignatureCheckpointKey, sdk.Uint64ToBigEndian(end)...)
+	iter := store.Iterator(startKey, endKey)
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		// cb returns true to stop early
+		if cb(iter.Key()[9:]) {
+			break
+		}
 	}
 }
--- x/crosschain/types/key.go
+++ x/crosschain/types/key.go
@@ -182,6 +182,6 @@ func GetTokenToDenomKey(denom string) []byte {
 }
 
 // GetPastExternalSignatureCheckpointKey returns the following key format
-func GetPastExternalSignatureCheckpointKey(checkpoint []byte) []byte {
-	return append(PastExternalSignatureCheckpointKey, checkpoint...)
+func GetPastExternalSignatureCheckpointKey(blockHeight uint64, checkpoint []byte) []byte {
+	return append(PastExternalSignatureCheckpointKey, append(sdk.Uint64ToBigEndian(blockHeight), checkpoint...)...)
 }
