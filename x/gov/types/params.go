aebe4a0a83fef0afd323b544b5487a9fc2f1f856
overload-ak <39258960+overload-ak@users.noreply.github.com>
2023-03-01 09:50:29 +0800

    fix(gov): error code

--- x/gov/types/params.go
+++ x/gov/types/params.go
@@ -1,16 +1,15 @@
 package types
 
 import (
+	"strings"
+
 	sdkmath "cosmossdk.io/math"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	fxtypes "github.com/functionx/fx-core/v3/types"
-)
-
-const (
-	// ProposalTypeCommunityPoolSpend defines the type for a CommunityPoolSpendProposal
-	ProposalTypeCommunityPoolSpend = "CommunityPoolSpend"
-	CommunityPoolSpendByRouter     = "distribution"
 )
 
 func EGFProposalMinDeposit(claimCoin sdk.Coins) sdk.Coins {
@@ -23,10 +22,47 @@ func EGFProposalMinDeposit(claimCoin sdk.Coins) sdk.Coins {
 	if claimAmount.LTE(EGFDepositThreshold) {
 		return GetInitialDeposit()
 	}
-	initialDeposit := claimAmount.ToDec().Mul(ClaimRatio).TruncateInt()
+	initialDeposit := sdk.NewDecFromInt(claimAmount).Mul(ClaimRatio).TruncateInt()
 	return sdk.NewCoins(sdk.NewCoin(fxtypes.DefaultDenom, initialDeposit))
 }
 
 func GetInitialDeposit() sdk.Coins {
 	return sdk.NewCoins(sdk.NewCoin(fxtypes.DefaultDenom, sdkmath.NewInt(1000).Mul(sdkmath.NewInt(1e18))))
+}
+
+func CheckEGFProposalMsg(msgs []*codectypes.Any) (bool, sdk.Coins) {
+	for _, msg := range msgs {
+		// v1beta1 legacy MsgServer interface.from a legacy Content
+		if strings.EqualFold(msg.TypeUrl, sdk.MsgTypeURL(&govv1.MsgExecLegacyContent{})) {
+			legacyContent := msg.GetCachedValue().(*govv1.MsgExecLegacyContent)
+			content := legacyContent.GetContent()
+			if strings.EqualFold(content.TypeUrl, "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal") {
+				communityPoolSpendProposal := content.GetCachedValue().(*distributiontypes.CommunityPoolSpendProposal)
+				return true, communityPoolSpendProposal.Amount
+			}
+		}
+		// TODO v1 MsgServer MsgCommunityPoolSpend pending
+	}
+	return false, nil
+}
+
+func CheckErc20ProposalMsg(msgs []*codectypes.Any) bool {
+	for _, msg := range msgs {
+		// v1beta1 legacy MsgServer interface.from a legacy Content
+		if strings.EqualFold(msg.TypeUrl, sdk.MsgTypeURL(&govv1.MsgExecLegacyContent{})) {
+			legacyContent := msg.GetCachedValue().(*govv1.MsgExecLegacyContent)
+			content := legacyContent.GetContent()
+			switch content.TypeUrl {
+			case "/fx.erc20.v1.RegisterCoinProposal",
+				"/fx.erc20.v1.RegisterERC20Proposal",
+				"/fx.erc20.v1.ToggleTokenConversionProposal",
+				"/fx.erc20.v1.UpdateDenomAliasProposal":
+				return true
+			default:
+				return false
+			}
+		}
+		// TODO v1 MsgServer erc20 pending
+	}
+	return false
 }
