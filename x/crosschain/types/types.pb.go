f4ac51ec7c7c8e2aad28f784456d1a4b05153b79
devon <80245700+devon-chain@users.noreply.github.com>
2023-03-09 15:36:17 +0800

    build: deps buf.build/cosmos/ibc:9851f8a91ac6423f91cc2433bfda9495
    * deps buf.build/cosmos/gogo-proto:bee5511075b7499da6178d9e4aaa628b
    
    * deps buf.build/googleapis/googleapis:8d7204855ec14631a499bd7393ce1970
    
    * buf.build/cosmos/ics23:55085f7c710a45f58fa09947208eb70b

--- x/crosschain/types/types.pb.go
+++ x/crosschain/types/types.pb.go
@@ -8,7 +8,7 @@ import (
 	types "github.com/cosmos/cosmos-sdk/codec/types"
 	github_com_cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
 	types1 "github.com/cosmos/cosmos-sdk/types"
-	_ "github.com/cosmos/gogoproto/gogoproto"
+	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
