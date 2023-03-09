d83d187f4e3a43de319c8c38a89e02ccb75c1997
devon <80245700+devon-chain@users.noreply.github.com>
2023-03-09 14:03:11 +0800

    chore: remove unused code

--- x/migrate/types/errors.go
+++ x/migrate/types/errors.go
@@ -2,16 +2,14 @@ package types
 
 import errorsmod "cosmossdk.io/errors"
 
-// todo: remove unused code
-
 var (
-	ErrInvalidSignature = errorsmod.Register(ModuleName, 2, "invalid signature")
 	ErrInvalidAddress   = errorsmod.Register(ModuleName, 3, "invalid address")
 	ErrAlreadyMigrate   = errorsmod.Register(ModuleName, 4, "already migrate")
 	ErrMigrateValidate  = errorsmod.Register(ModuleName, 6, "migrate validate error")
 	ErrMigrateExecute   = errorsmod.Register(ModuleName, 7, "migrate execute error")
-	ErrSameAccount      = errorsmod.Register(ModuleName, 8, "same account")
 	ErrInvalidPublicKey = errorsmod.Register(ModuleName, 9, "invalid public key")
 
+	// ErrInvalidSignature = errorsmod.Register(ModuleName, 2, "invalid signature")
 	// InvalidRequest      = errorsmod.Register(ModuleName, 5, "invalid request")
+	// ErrSameAccount      = errorsmod.Register(ModuleName, 8, "same account")
 )
