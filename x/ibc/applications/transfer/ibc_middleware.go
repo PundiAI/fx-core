796fad04d7b858a603091ade8e5bb7f28a1dbb39
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2023-04-17 17:57:16 +0800

    fix: event ibc OnRecv unmarshal packetData error

--- x/ibc/applications/transfer/ibc_middleware.go
+++ x/ibc/applications/transfer/ibc_middleware.go
@@ -6,7 +6,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
@@ -64,20 +64,21 @@ func (im IBCMiddleware) OnRecvPacket(
 	ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)})
 
 	var data types.FungibleTokenPacketData
+	var ackErr error
 	if err := types.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
-		ack = channeltypes.NewErrorAcknowledgement(fmt.Errorf("cannot unmarshal ICS-20 transfer packet data"))
+		ackErr = sdkerrors.ErrInvalidType.Wrap("cannot unmarshal ICS-20 transfer packet data")
+		ack = channeltypes.NewErrorAcknowledgement(ackErr)
 	}
 
 	// only attempt the application logic if the packet data
 	// was successfully decoded
-	var err error
 	if ack.Success() {
 		if len(data.GetFee()) == 0 {
 			data.Fee = sdkmath.ZeroInt().String()
 		}
-		err = im.keeper.OnRecvPacket(ctx, packet, data)
-		if err != nil {
+		if err := im.keeper.OnRecvPacket(ctx, packet, data); err != nil {
 			ack = channeltypes.NewErrorAcknowledgement(err)
+			ackErr = err
 		}
 	}
 
@@ -92,8 +93,8 @@ func (im IBCMiddleware) OnRecvPacket(
 		sdk.NewAttribute(transfertypes.AttributeKeyAckSuccess, fmt.Sprintf("%t", ack.Success())),
 	)
 
-	if err != nil {
-		event = event.AppendAttributes(sdk.NewAttribute(types.AttributeKeyRecvError, err.Error()))
+	if ackErr != nil {
+		event = event.AppendAttributes(sdk.NewAttribute(types.AttributeKeyRecvError, ackErr.Error()))
 	}
 	ctx.EventManager().EmitEvent(event)
 
@@ -110,11 +111,11 @@ func (im IBCMiddleware) OnAcknowledgementPacket(
 ) error {
 	var ack channeltypes.Acknowledgement
 	if err := types.ModuleCdc.UnmarshalJSON(acknowledgement, &ack); err != nil {
-		return errorsmod.Wrapf(errortypes.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet acknowledgement: %v", err)
+		return errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet acknowledgement: %v", err)
 	}
 	var data types.FungibleTokenPacketData
 	if err := types.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
-		return errorsmod.Wrapf(errortypes.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
+		return errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
 	}
 	if err := im.keeper.OnAcknowledgementPacket(ctx, packet, data, ack); err != nil {
 		return err
@@ -155,7 +156,7 @@ func (im IBCMiddleware) OnTimeoutPacket(
 ) error {
 	var data types.FungibleTokenPacketData
 	if err := types.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
-		return errorsmod.Wrapf(errortypes.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
+		return errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "cannot unmarshal ICS-20 transfer packet data: %s", err.Error())
 	}
 	// refund tokens
 	if err := im.keeper.OnTimeoutPacket(ctx, packet, data); err != nil {
