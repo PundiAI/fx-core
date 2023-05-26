262099601c2477e32f69368a0a7da95d72607b6a
zakir <80246097+zakir-code@users.noreply.github.com>
2023-05-26 11:36:23 +0800

    style: remove unused code

--- app/upgrades/types.go
+++ app/upgrades/types.go
@@ -14,7 +14,7 @@ import (
 // An upgrade must implement this struct, and then set it in the app.go.
 // The app.go will then define the handler.
 type Upgrade struct {
-	// Upgrade version name, for the upgrade handler, e.g. `v7`
+	// Upgrade version name, for the upgrade handler
 	UpgradeName string
 
 	// CreateUpgradeHandler defines the function that creates an upgrade handler
@@ -24,19 +24,13 @@ type Upgrade struct {
 	StoreUpgrades func() *storetypes.StoreUpgrades
 }
 
-type Upgrades []Upgrade
-
-func (u Upgrades) GetLatest() Upgrade {
-	return u[len(u)-1]
-}
-
 // Fork defines a struct containing the requisite fields for a non-software upgrade proposal
 // Hard Fork at a given height to implement.
 // There is one time code that can be added for the start of the Fork, in `BeginForkLogic`.
 // Any other change in the code should be height-gated, if the goal is to have old and new binaries
 // to be compatible prior to the upgrade height.
 type Fork struct {
-	// Upgrade version name, for the upgrade handler, e.g. `v7`
+	// Upgrade version name, for the upgrade handler
 	UpgradeName string
 	// height the upgrade occurs at
 	UpgradeHeight int64
