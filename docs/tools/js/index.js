3409634ccc7f8e3d8a58902635cfeaf833a01faf
zakir <80246097+zakir-code@users.noreply.github.com>
2023-05-26 18:39:22 +0800

    perf(tools): style and experience
    fix(tools): calculate the average block time

--- docs/tools/js/index.js
+++ docs/tools/js/index.js
@@ -27,6 +27,15 @@ const groupBy = (arr, property) => {
     }, {});
 };
 
+function getStarscanUrl() {
+    let starscanUrl = "https://starscan.io";
+    const network = getQueryVariable("network")
+    if (network === "testnet") {
+        starscanUrl = "https://testnet.starscan.io";
+    }
+    return starscanUrl
+}
+
 function httpGetJson(theUrl) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, false); // false for synchronous request
@@ -38,70 +47,77 @@ function httpGetJson(theUrl) {
     return response;
 }
 
-function getFxRestUrl(){
-    let fxRestUrl = "https://fx-rest.functionx.io";
+function getRestUrl() {
+    let restUrl = "https://fx-rest.functionx.io";
     const network = getQueryVariable("network")
     if (network === "testnet") {
-        fxRestUrl = "https://testnet-fx-rest.functionx.io";
+        restUrl = "https://testnet-fx-rest.functionx.io";
     }
-    return fxRestUrl
+    return restUrl
 }
 
-function getFxJsonrpcUrl(){
-    let fxJsonrpcUrl = "https://fx-json.functionx.io:26657";
+function getJsonrpcUrl() {
+    let jsonrpcUrl = "https://fx-json.functionx.io:26657";
     const network = getQueryVariable("network")
     if (network === "testnet") {
-        fxJsonrpcUrl = "https://testnet-fx-json.functionx.io:26657";
+        jsonrpcUrl = "https://testnet-fx-json.functionx.io:26657";
     }
-    return fxJsonrpcUrl
-}
-
-function getAverageBlockTime(block) {
-    const nowHeight = Number(block.block.header.height);
-    const nowTime = block.block.header.time;
-    const oldBlock = httpGetJson(getFxJsonrpcUrl() + "/block?height=" + (nowHeight - 100).toString());
-    const oldTime = oldBlock.block.header.time;
-    const avgBlockTime = Math.round((new Date(nowTime) - new Date(oldTime)) / 100);
-    console.log("Average BlockTime", avgBlockTime);
-    return avgBlockTime;
+    return jsonrpcUrl
 }
 
 function getValidators() {
-    const result = httpGetJson(getFxRestUrl() + "/cosmos/staking/v1beta1/validators")
+    const result = httpGetJson(getRestUrl() + "/cosmos/staking/v1beta1/validators")
     return result.validators
 }
 
 function getSigningInfos() {
-    const result = httpGetJson(getFxRestUrl() + "/cosmos/slashing/v1beta1/signing_infos")
+    const result = httpGetJson(getRestUrl() + "/cosmos/slashing/v1beta1/signing_infos")
     return result.info
 }
 
-function getBlockSignatures() {
-    const result = httpGetJson(getFxJsonrpcUrl() + "/block")
-    return result.block.last_commit.signatures
-}
-
-function getPeers() {
-    const result = httpGetJson(getFxJsonrpcUrl() + "/net_info")
-    return result.peers
-}
-
 function getBalances(address) {
-    const result = httpGetJson(getFxRestUrl() + `/cosmos/bank/v1beta1/balances/${address}`)
+    if (address) {
+        return getTotalSupply()
+    }
+    const result = httpGetJson(getRestUrl() + `/cosmos/bank/v1beta1/balances/${address}`)
     return result.balances
 }
 
 function getTotalSupply() {
-    const result = httpGetJson(getFxRestUrl() + `/cosmos/bank/v1beta1/supply`)
+    const result = httpGetJson(getRestUrl() + `/cosmos/bank/v1beta1/supply`)
     return result.supply
 }
 
 function getMetadatas() {
-    const result = httpGetJson(getFxRestUrl() + `/cosmos/bank/v1beta1/denoms_metadata`)
+    const result = httpGetJson(getRestUrl() + `/cosmos/bank/v1beta1/denoms_metadata`)
     return result.metadatas
 }
 
 function getCurrentPlan() {
-    const result = httpGetJson(getFxRestUrl() + `/cosmos/upgrade/v1beta1/current_plan`)
+    const result = httpGetJson(getRestUrl() + `/cosmos/upgrade/v1beta1/current_plan`)
     return result.plan
+}
+
+function getBlock(height) {
+    const result = httpGetJson(getJsonrpcUrl() + "/block" + (height ? "?height=" + height : ""))
+    return result.block
+}
+
+function getBlockSignatures() {
+    return getBlock().last_commit.signatures
+}
+
+function getPeers() {
+    const result = httpGetJson(getJsonrpcUrl() + "/net_info")
+    return result.peers
+}
+
+function getAverageBlockTime(block) {
+    const nowHeight = Number(block.header.height);
+    const nowTime = block.header.time;
+    const oldBlock = getBlock(nowHeight - 20000);
+    const oldTime = oldBlock.header.time;
+    const avgBlockTime = Math.round((new Date(nowTime) - new Date(oldTime)) / 20000);
+    console.log("Average BlockTime", avgBlockTime);
+    return avgBlockTime;
 }
