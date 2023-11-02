ea9688bcfc3f965e87d6a70ec7493691ba0cd872
zakir <80246097+zakir-code@users.noreply.github.com>
2023-11-02 11:44:16 +0800

    add unit test

--- app/upgrades/v6/upgrade.go
+++ app/upgrades/v6/upgrade.go
@@ -17,36 +17,9 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		cacheCtx, commit := ctx.CacheContext()
 
-		mintParams := app.MintKeeper.GetParams(cacheCtx)
-		mintParams.InflationMax = sdk.ZeroDec()
-		mintParams.InflationMin = sdk.ZeroDec()
-		app.MintKeeper.SetParams(cacheCtx, mintParams)
-
-		distrParams := app.DistrKeeper.GetParams(cacheCtx)
-		distrParams.CommunityTax = sdk.ZeroDec()
-		distrParams.BaseProposerReward = sdk.ZeroDec()
-		distrParams.BonusProposerReward = sdk.ZeroDec()
-		app.DistrKeeper.SetParams(cacheCtx, distrParams)
-
-		stakingParams := app.StakingKeeper.GetParams(cacheCtx)
-		stakingParams.UnbondingTime = 0
-		app.StakingKeeper.SetParams(cacheCtx, stakingParams)
-
-		govTallyParams := app.GovKeeper.GetTallyParams(cacheCtx)
-		govTallyParams.Quorum = sdk.OneDec().String()        // 100%
-		govTallyParams.Threshold = sdk.OneDec().String()     // 100%
-		govTallyParams.VetoThreshold = sdk.OneDec().String() // 100%
-		app.GovKeeper.SetTallyParams(cacheCtx, govTallyParams)
-
-		app.GovKeeper.IterateParams(cacheCtx, func(param *govtypes.Params) (stop bool) {
-			param.Quorum = sdk.OneDec().String()        // 100%
-			param.Threshold = sdk.OneDec().String()     // 100%
-			param.VetoThreshold = sdk.OneDec().String() // 100%
-			if err := app.GovKeeper.SetParams(cacheCtx, param); err != nil {
-				panic(err)
-			}
-			return false
-		})
+		if err := UpdateParams(cacheCtx, app); err != nil {
+			return nil, err
+		}
 
 		ctx.Logger().Info("start to run v6 migrations...", "module", "upgrade")
 		toVM, err := mm.RunMigrations(cacheCtx, configurator, fromVM)
@@ -58,4 +31,50 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Upgrade complete")
 		return toVM, nil
 	}
+}
+
+func UpdateParams(cacheCtx sdk.Context, app *keepers.AppKeepers) error {
+	mintParams := app.MintKeeper.GetParams(cacheCtx)
+	mintParams.InflationMax = sdk.ZeroDec()
+	mintParams.InflationMin = sdk.ZeroDec()
+	if err := mintParams.Validate(); err != nil {
+		return err
+	}
+	app.MintKeeper.SetParams(cacheCtx, mintParams)
+
+	distrParams := app.DistrKeeper.GetParams(cacheCtx)
+	distrParams.CommunityTax = sdk.ZeroDec()
+	distrParams.BaseProposerReward = sdk.ZeroDec()
+	distrParams.BonusProposerReward = sdk.ZeroDec()
+	if err := distrParams.ValidateBasic(); err != nil {
+		return err
+	}
+	app.DistrKeeper.SetParams(cacheCtx, distrParams)
+
+	stakingParams := app.StakingKeeper.GetParams(cacheCtx)
+	stakingParams.UnbondingTime = 1
+	if err := stakingParams.Validate(); err != nil {
+		return err
+	}
+	app.StakingKeeper.SetParams(cacheCtx, stakingParams)
+
+	govTallyParams := app.GovKeeper.GetTallyParams(cacheCtx)
+	govTallyParams.Quorum = sdk.OneDec().String()        // 100%
+	govTallyParams.Threshold = sdk.OneDec().String()     // 100%
+	govTallyParams.VetoThreshold = sdk.OneDec().String() // 100%
+	app.GovKeeper.SetTallyParams(cacheCtx, govTallyParams)
+
+	app.GovKeeper.IterateParams(cacheCtx, func(param *govtypes.Params) (stop bool) {
+		param.Quorum = sdk.OneDec().String()        // 100%
+		param.Threshold = sdk.OneDec().String()     // 100%
+		param.VetoThreshold = sdk.OneDec().String() // 100%
+		if err := param.ValidateBasic(); err != nil {
+			panic(err)
+		}
+		if err := app.GovKeeper.SetParams(cacheCtx, param); err != nil {
+			panic(err)
+		}
+		return false
+	})
+	return nil
 }
