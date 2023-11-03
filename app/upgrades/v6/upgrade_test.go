39d149734fdb9d95cdc808f31043588a47d3f027
zakir <80246097+zakir-code@users.noreply.github.com>
2023-11-03 16:52:50 +0800

    feat(upgrade): migrate delegate (#120)

--- app/upgrades/v6/upgrade_test.go
+++ app/upgrades/v6/upgrade_test.go
@@ -5,10 +5,11 @@ import (
 	"strings"
 	"testing"
 
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/suite"
-	abci "github.com/tendermint/tendermint/abci/types"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
@@ -17,13 +18,15 @@ import (
 	"github.com/functionx/fx-core/v6/testutil/helpers"
 	fxtypes "github.com/functionx/fx-core/v6/types"
 	layer2types "github.com/functionx/fx-core/v6/x/layer2/types"
+	migratetypes "github.com/functionx/fx-core/v6/x/migrate/types"
 )
 
 type UpgradeTestSuite struct {
 	suite.Suite
 
-	app *app.App
-	ctx sdk.Context
+	app       *app.App
+	ctx       sdk.Context
+	valNumber int
 }
 
 func TestUpgradeTestSuite(t *testing.T) {
@@ -31,8 +34,8 @@ func TestUpgradeTestSuite(t *testing.T) {
 }
 
 func (s *UpgradeTestSuite) SetupTest() {
-	valNumber := tmrand.Intn(5) + 5
-	valSet, valAccounts, valBalances := helpers.GenerateGenesisValidator(valNumber, sdk.Coins{})
+	s.valNumber = tmrand.Intn(5) + 5
+	valSet, valAccounts, valBalances := helpers.GenerateGenesisValidator(s.valNumber, sdk.Coins{})
 	s.app = helpers.SetupWithGenesisValSet(s.T(), valSet, valAccounts, valBalances...)
 	s.ctx = s.app.NewContext(false, tmproto.Header{
 		ChainID:         fxtypes.MainnetChainId,
@@ -46,22 +49,7 @@ func (s *UpgradeTestSuite) SetupTest() {
 }
 
 func (s *UpgradeTestSuite) CommitBlock(block int64) {
-	_ = s.app.Commit()
-	block--
-
-	ctx := s.ctx
-	nextBlockHeight := ctx.BlockHeight() + 1
-
-	_ = s.app.BeginBlock(abci.RequestBeginBlock{
-		Header: tmproto.Header{
-			Height: nextBlockHeight,
-		},
-	})
-	s.ctx = s.app.NewContext(false, ctx.BlockHeader())
-	s.ctx = s.ctx.WithBlockHeight(nextBlockHeight)
-	if block > 0 {
-		s.CommitBlock(block)
-	}
+	helpers.MintBlock(s.app, s.ctx, block)
 }
 
 func (s *UpgradeTestSuite) TestUpdateParams() {
@@ -95,4 +83,34 @@ func (s *UpgradeTestSuite) TestMigrateLayer2Module() {
 		s.Equal(bridgeToken.Token, address)
 		s.Equal(bridgeToken.Denom, fmt.Sprintf("%s%s", layer2types.ModuleName, address))
 	}
+}
+
+func (s *UpgradeTestSuite) TestAutoUndelegate_And_ExportDelegate() {
+	delPrivKey := helpers.NewPriKey()
+	delAddr := sdk.AccAddress(delPrivKey.PubKey().Address())
+	helpers.AddTestAddr(s.app, s.ctx, delAddr, sdk.NewCoins(sdk.NewCoin(fxtypes.DefaultDenom, sdk.NewInt(10000))))
+	account := s.app.AccountKeeper.GetAccount(s.ctx, delAddr)
+	s.NoError(account.SetPubKey(delPrivKey.PubKey()))
+	s.app.AccountKeeper.SetAccount(s.ctx, account)
+
+	validators := s.app.StakingKeeper.GetAllValidators(s.ctx)
+	s.Equal(s.valNumber, len(validators))
+	validator := validators[0]
+
+	_, err := s.app.StakingKeeper.Delegate(s.ctx, delAddr, sdkmath.NewInt(100), stakingtypes.Unbonded, validator, true)
+	s.NoError(err)
+
+	newPrivKey := helpers.NewEthPrivKey()
+	_, err = s.app.MigrateKeeper.MigrateAccount(s.ctx, &migratetypes.MsgMigrateAccount{
+		From:      delAddr.String(),
+		To:        common.BytesToAddress(newPrivKey.PubKey().Address()).String(),
+		Signature: "",
+	})
+	s.NoError(err)
+
+	delegations := v6.AutoUndelegate(s.ctx, s.app.StakingKeeper)
+	s.Equal(s.valNumber+1, len(delegations))
+
+	delegations = v6.ExportDelegate(s.ctx, s.app.MigrateKeeper, delegations)
+	s.Equal(1, len(delegations))
 }
