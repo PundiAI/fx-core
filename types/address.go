83743068753ca3bb7682d09dcb0450dec68ec55a
zakir <80246097+zakir-code@users.noreply.github.com>
2024-02-28 16:47:52 +0800

    feat: add address to str function (#236)

--- types/address.go
+++ types/address.go
@@ -6,6 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/bech32"
 	"github.com/ethereum/go-ethereum/common"
+	tronaddress "github.com/fbsobreira/gotron-sdk/pkg/address"
 )
 
 func ParseAddress(addr string) (accAddr sdk.AccAddress, isEvmAddr bool, err error) {
@@ -18,4 +19,12 @@ func ParseAddress(addr string) (accAddr sdk.AccAddress, isEvmAddr bool, err erro
 		return common.HexToAddress(addr).Bytes(), true, nil
 	}
 	return nil, false, errors.Join(decodeErr, ethAddrError)
+}
+
+func AddressToStr(bt []byte, module string) string {
+	if module == "tron" {
+		return tronaddress.Address(bt).String()
+	} else {
+		return common.BytesToAddress(bt).String()
+	}
 }
