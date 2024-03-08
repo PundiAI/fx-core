75433e8932dfce9c95c742f658d30c40119e7977
zakir <80246097+zakir-code@users.noreply.github.com>
2024-03-08 13:44:16 +0800

    style: remove unused params code (#255)

--- x/crosschain/types/params.go
+++ x/crosschain/types/params.go
@@ -6,7 +6,6 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	fxtypes "github.com/functionx/fx-core/v7/types"
 )
@@ -30,40 +29,7 @@ var (
 	AttestationVotesPowerThreshold = sdkmath.NewInt(66)
 
 	AttestationProposalOracleChangePowerThreshold = sdkmath.NewInt(30)
-
-	// ParamsStoreKeyGravityID stores the gravity id
-	ParamsStoreKeyGravityID = []byte("GravityID")
-
-	// ParamsStoreKeyAverageBlockTime stores the signed blocks window
-	ParamsStoreKeyAverageBlockTime = []byte("AverageBlockTime")
-
-	// ParamsStoreKeyExternalBatchTimeout stores the signed blocks window
-	ParamsStoreKeyExternalBatchTimeout = []byte("ExternalBatchTimeout")
-
-	// ParamsStoreKeyAverageExternalBlockTime stores the signed blocks window
-	ParamsStoreKeyAverageExternalBlockTime = []byte("AverageExternalBlockTime")
-
-	// ParamsStoreKeySignedWindow stores the signed blocks window
-	ParamsStoreKeySignedWindow = []byte("SignedWindow")
-
-	// ParamsStoreSlashFraction stores the slash fraction oracle set
-	ParamsStoreSlashFraction = []byte("SlashFraction")
-
-	// ParamStoreOracleSetUpdatePowerChangePercent oracle set update power change percent
-	ParamStoreOracleSetUpdatePowerChangePercent = []byte("OracleSetUpdatePowerChangePercent")
-
-	// ParamStoreIbcTransferTimeoutHeight gravity and ibc transfer timeout height
-	ParamStoreIbcTransferTimeoutHeight = []byte("IbcTransferTimeoutHeight")
-
-	// ParamOracleDelegateThreshold stores the oracle delegate threshold
-	ParamOracleDelegateThreshold = []byte("OracleDelegateThreshold")
-
-	// ParamOracleDelegateMultiple stores the oracle delegate multiple
-	ParamOracleDelegateMultiple = []byte("OracleDelegateMultiple")
 )
-
-// Ensure that params implements the proper interface
-var _ paramtypes.ParamSet = &Params{}
 
 func DefaultParams() Params {
 	return Params{
@@ -114,28 +80,6 @@ func (m *Params) ValidateBasic() error {
 		return errorsmod.Wrap(err, "delegate multiple")
 	}
 	return nil
-}
-
-// ParamKeyTable for auth module
-func ParamKeyTable() paramtypes.KeyTable {
-	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
-}
-
-// ParamSetPairs implements the ParamSet interface and returns all the key/value pairs
-// pairs of auth module's parameters.
-func (m *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{
-		paramtypes.NewParamSetPair(ParamsStoreKeyGravityID, &m.GravityId, validateGravityID),
-		paramtypes.NewParamSetPair(ParamsStoreKeyAverageBlockTime, &m.AverageBlockTime, validateAverageBlockTime),
-		paramtypes.NewParamSetPair(ParamsStoreKeyExternalBatchTimeout, &m.ExternalBatchTimeout, validateExternalBatchTimeout),
-		paramtypes.NewParamSetPair(ParamsStoreKeyAverageExternalBlockTime, &m.AverageExternalBlockTime, validateAverageExternalBlockTime),
-		paramtypes.NewParamSetPair(ParamsStoreKeySignedWindow, &m.SignedWindow, validateSignedWindow),
-		paramtypes.NewParamSetPair(ParamsStoreSlashFraction, &m.SlashFraction, validateSlashFraction),
-		paramtypes.NewParamSetPair(ParamStoreOracleSetUpdatePowerChangePercent, &m.OracleSetUpdatePowerChangePercent, validateOracleSetUpdatePowerChangePercent),
-		paramtypes.NewParamSetPair(ParamStoreIbcTransferTimeoutHeight, &m.IbcTransferTimeoutHeight, validateIbcTransferTimeoutHeight),
-		paramtypes.NewParamSetPair(ParamOracleDelegateThreshold, &m.DelegateThreshold, validateOracleDelegateThreshold),
-		paramtypes.NewParamSetPair(ParamOracleDelegateMultiple, &m.DelegateMultiple, validateOracleDelegateMultiple),
-	}
 }
 
 func validateGravityID(i interface{}) error {
--- x/erc20/types/params.go
+++ x/erc20/types/params.go
@@ -3,23 +3,7 @@ package types
 import (
 	"fmt"
 	"time"
-
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
-
-// Parameter store key
-var (
-	ParamStoreKeyEnableErc20   = []byte("EnableErc20")
-	ParamStoreKeyEnableEVMHook = []byte("EnableEVMHook")
-	ParamStoreKeyIBCTimeout    = []byte("IBCTimeout")
-)
-
-var _ paramtypes.ParamSet = &Params{}
-
-// ParamKeyTable returns the parameter key table.
-func ParamKeyTable() paramtypes.KeyTable {
-	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
-}
 
 // NewParams creates a new Params object
 func NewParams(enableErc20 bool, enableEVMHook bool, ibcTimeout time.Duration) Params {
@@ -35,33 +19,6 @@ func DefaultParams() Params {
 		EnableErc20:   true,
 		EnableEVMHook: true,
 		IbcTimeout:    12 * time.Hour,
-	}
-}
-
-func validateBool(i interface{}) error {
-	_, ok := i.(bool)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	return nil
-}
-
-func validateTimeDuration(i interface{}) error {
-	_, ok := i.(time.Duration)
-	if !ok {
-		return fmt.Errorf("invalid parameter type: %T", i)
-	}
-
-	return nil
-}
-
-// ParamSetPairs returns the parameter set pairs.
-func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{
-		paramtypes.NewParamSetPair(ParamStoreKeyEnableErc20, &p.EnableErc20, validateBool),
-		paramtypes.NewParamSetPair(ParamStoreKeyEnableEVMHook, &p.EnableEVMHook, validateBool),
-		paramtypes.NewParamSetPair(ParamStoreKeyIBCTimeout, &p.IbcTimeout, validateTimeDuration),
 	}
 }
 
