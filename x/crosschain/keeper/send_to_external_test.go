fc91f9dc4077541553b8a2f920b17e5712b3b111
zakir <80246097+zakir-code@users.noreply.github.com>
2024-05-07 20:02:57 +0800

    test: add send to external test (#420)
    Co-authored-by: fx0x55 <80245546+fx0x55@users.noreply.github.com>

---
+++ x/crosschain/keeper/send_to_external_test.go
@@ -0,0 +1,55 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	"go.uber.org/mock/gomock"
+
+	"github.com/functionx/fx-core/v7/testutil/helpers"
+	"github.com/functionx/fx-core/v7/x/crosschain/types"
+	erc20types "github.com/functionx/fx-core/v7/x/erc20/types"
+)
+
+func (s *KeeperTestSuite) TestSendToExternal() {
+	bridgeToken := types.BridgeToken{
+		Token: "0x1",
+		Denom: "eth0xusdt",
+	}
+	s.crosschainKeeper.AddBridgeToken(s.ctx, bridgeToken.Token, bridgeToken.Denom)
+	senderAddr := sdk.AccAddress(helpers.GenerateAddress().Bytes())
+	sendMsg := types.MsgSendToExternal{
+		Sender:    senderAddr.String(),
+		Amount:    sdk.NewCoin("usdt", sdk.NewInt(int64(tmrand.Uint32()))),
+		BridgeFee: sdk.NewCoin("usdt", sdk.NewInt(int64(tmrand.Uint32()))),
+		ChainName: s.moduleName,
+	}
+	s.erc20Keeper.EXPECT().ConvertDenomToTarget(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(sdk.NewCoin("eth0xusdt", sendMsg.Amount.Amount), erc20types.ErrInsufficientLiquidity).
+		Times(1)
+	external, err := s.msgServer.SendToExternal(s.ctx, &sendMsg)
+	require.NoError(s.T(), err)
+	require.EqualValues(s.T(), 1, external.OutgoingTxId)
+
+	pendingSendToExternal, found := s.crosschainKeeper.GetPendingPoolTxById(s.ctx, external.OutgoingTxId)
+	require.True(s.T(), found)
+	require.EqualValues(s.T(), sendMsg.Amount, pendingSendToExternal.Token)
+	require.EqualValues(s.T(), sendMsg.BridgeFee, pendingSendToExternal.Fee)
+
+	// add liquidity
+	erc20ModuleAddr := sdk.AccAddress(helpers.GenerateAddress().Bytes())
+	s.accountKeeper.EXPECT().GetModuleAddress(erc20types.ModuleName).Return(erc20ModuleAddr).Times(1)
+	sendToken := sdk.NewCoin(bridgeToken.Denom, sendMsg.Amount.Amount.Add(sendMsg.BridgeFee.Amount))
+	s.bankKeeper.EXPECT().HasBalance(gomock.Any(), erc20ModuleAddr, sendToken).Return(true).Times(1)
+	s.bankKeeper.EXPECT().SendCoinsFromModuleToAccount(gomock.Any(), erc20types.ModuleName, senderAddr, sdk.NewCoins(sendToken)).Return(nil).Times(1)
+	s.erc20Keeper.EXPECT().IsOriginOrConvertedDenom(gomock.Any(), bridgeToken.Denom).Return(false).Times(1)
+
+	s.bankKeeper.EXPECT().SendCoinsFromAccountToModule(gomock.Any(), senderAddr, s.moduleName, sdk.NewCoins(sendToken)).Return(nil).Times(1)
+	s.bankKeeper.EXPECT().BurnCoins(gomock.Any(), s.moduleName, sdk.NewCoins(sendToken)).Return(nil).Times(1)
+
+	s.crosschainKeeper.HandlePendingOutgoingTx(s.ctx, helpers.GenerateAddress().Bytes(), 1, &bridgeToken)
+
+	// check pending send to external tx is removed
+	_, found = s.crosschainKeeper.GetPendingPoolTxById(s.ctx, external.OutgoingTxId)
+	s.Require().False(found)
+}
