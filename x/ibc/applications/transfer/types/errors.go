71b6b3c8a23caea5917d75dc76e98366353b452c
zakir <80246097+zakir-code@users.noreply.github.com>
2024-05-09 15:19:09 +0800

    perf: remove gas limit field when ibc call

--- x/ibc/applications/transfer/types/errors.go
+++ x/ibc/applications/transfer/types/errors.go
@@ -8,5 +8,4 @@ var (
 	ErrFeeDenomNotMatchTokenDenom = errorsmod.Register(ModuleName, 100, "invalid fee denom, must match token denom")
 	ErrRouterNotFound             = errorsmod.Register(ModuleName, 103, "router not found")
 	ErrMemoNotSupport             = errorsmod.Register(ModuleName, 104, "memo not support")
-	ErrInvalidEvmData             = errorsmod.Register(ModuleName, 105, "invalid evm data")
 )
