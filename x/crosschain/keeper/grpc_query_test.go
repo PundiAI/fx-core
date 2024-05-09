7d90c3bd14a5539ee291c4113346435b15e8ec55
zakir <80246097+zakir-code@users.noreply.github.com>
2024-05-09 21:08:59 +0800

    feat: query bridge calls (#434)
    Co-authored-by: fx0x55 <80245546+fx0x55@users.noreply.github.com>

---
+++ x/crosschain/keeper/grpc_query_test.go
@@ -0,0 +1,46 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+
+	"github.com/functionx/fx-core/v7/testutil/helpers"
+	"github.com/functionx/fx-core/v7/x/crosschain/types"
+)
+
+func (s *KeeperTestSuite) TestKeeper_BridgeCalls() {
+	ctx := sdk.WrapSDKContext(s.ctx)
+	data1 := types.OutgoingBridgeCall{
+		Nonce:  tmrand.Uint64(),
+		Sender: sdk.AccAddress(helpers.GenerateAddress().Bytes()).String(),
+	}
+	data2 := types.OutgoingBridgeCall{
+		Nonce:  tmrand.Uint64(),
+		Sender: sdk.AccAddress(helpers.GenerateAddress().Bytes()).String(),
+	}
+
+	s.crosschainKeeper.SetOutgoingBridgeCall(s.ctx, &data1)
+	s.crosschainKeeper.SetOutgoingBridgeCall(s.ctx, &data2)
+	actual, err := s.crosschainKeeper.BridgeCalls(ctx, &types.QueryBridgeCallsRequest{
+		ChainName: s.moduleName,
+		Pagination: &query.PageRequest{
+			Offset:     0,
+			Limit:      1,
+			CountTotal: false,
+		},
+	})
+	s.NoError(err)
+	s.Equal(len(actual.BridgeCalls), 1)
+
+	actual, err = s.crosschainKeeper.BridgeCalls(ctx, &types.QueryBridgeCallsRequest{
+		ChainName: s.moduleName,
+		Pagination: &query.PageRequest{
+			Offset:     0,
+			Limit:      2,
+			CountTotal: false,
+		},
+	})
+	s.NoError(err)
+	s.Equal(len(actual.BridgeCalls), 2)
+}
