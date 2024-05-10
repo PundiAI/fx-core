67ff932006f4b3f72fa3790059dcda450b061428
zakir <80246097+zakir-code@users.noreply.github.com>
2024-05-10 09:48:49 +0800

    refactor(solidity): remove bridgecall gaslimit (#435)
    Co-authored-by: nulnut <151493716+nulnut@users.noreply.github.com>

--- solidity/test/bridge_call.ts
+++ solidity/test/bridge_call.ts
@@ -57,7 +57,6 @@ describe("bridge call tests", function () {
 
     describe("BridgeCallUpgradeable test", function () {
         const fxcoreChainId = "530";
-        const gasLimit = "3000000";
 
         it("should bridge call erc20", async function () {
             const tokens = [await erc20Token.getAddress()]
@@ -69,7 +68,7 @@ describe("bridge call tests", function () {
             const lastEventNonce = await fxBridge.state_lastEventNonce()
 
             // bridge call
-            await fxBridge.bridgeCall(fxcoreChainId, gasLimit, user1.address, user1.address, tokens, amounts, "0x", 0)
+            await fxBridge.bridgeCall(fxcoreChainId, user1.address, user1.address, tokens, amounts, "0x", 0)
 
             // check nonce
             const lastEventNonceAfter = await fxBridge.state_lastEventNonce()
@@ -113,7 +112,7 @@ describe("bridge call tests", function () {
                 const tokens = [await token1.getAddress(), await token2.getAddress()]
                 const amounts = [BigInt(1), BigInt(2)]
 
-                await fxBridge.bridgeCall(fxcoreChainId, gasLimit, user1.address, user1.address, tokens, amounts, "0x", 0)
+                await fxBridge.bridgeCall(fxcoreChainId, user1.address, user1.address, tokens, amounts, "0x", 0)
 
                 const balance1 = await token1.balanceOf(deploy.address)
                 const balance2 = await token2.balanceOf(deploy.address)
@@ -129,13 +128,13 @@ describe("bridge call tests", function () {
                 const tokens = [await token1.getAddress(), await token2.getAddress(), await token3.getAddress()]
                 const amounts = [BigInt(1), BigInt(2), BigInt(3)]
 
-                await fxBridge.bridgeCall(fxcoreChainId, gasLimit, user1.address, user1.address, tokens, amounts, "0x", 0)
+                await fxBridge.bridgeCall(fxcoreChainId, user1.address, user1.address, tokens, amounts, "0x", 0)
             })
             it("bridge call transfer 4 token", async function () {
                 const tokens = [await token1.getAddress(), await token2.getAddress(), await token3.getAddress(), await token4.getAddress()]
                 const amounts = [BigInt(1), BigInt(2), BigInt(3), BigInt(4)]
 
-                await fxBridge.bridgeCall(fxcoreChainId, gasLimit, user1.address, user1.address, tokens, amounts, "0x", 0)
+                await fxBridge.bridgeCall(fxcoreChainId, user1.address, user1.address, tokens, amounts, "0x", 0)
             })
         })
     })
