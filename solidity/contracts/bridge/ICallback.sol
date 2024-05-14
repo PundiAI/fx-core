4d7f785ed0bc9242de839503c8e55cd32949e0bf
nulnut <151493716+nulnut@users.noreply.github.com>
2024-05-14 10:03:05 +0800

    feat(solidity): refund callback

--- solidity/contracts/bridge/ICallback.sol
+++ solidity/contracts/bridge/ICallback.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/* solhint-disable one-contract-per-file */
 interface IBridgeCallback {
     function bridgeCallback(
         address,
@@ -9,5 +10,14 @@ interface IBridgeCallback {
         uint256[] memory,
         bytes memory,
         bytes memory
+    ) external;
+}
+
+/* solhint-disable one-contract-per-file */
+interface IRefundCallback {
+    function refundCallback(
+        uint256,
+        address[] memory,
+        uint256[] memory
     ) external;
 }
