7c336d076a917ef3555aa95fe10859975bc442be
zakir <80246097+zakir-code@users.noreply.github.com>
2024-05-27 15:02:34 +0800

    style(solidity): format typescript (#515)
    Co-authored-by: nulnut <151493716+nulnut@users.noreply.github.com>

--- solidity/test/fx_bridge.ts
+++ solidity/test/fx_bridge.ts
@@ -1,109 +1,128 @@
-import {ethers} from "hardhat";
-import {expect} from "chai";
-import {IFxBridgeLogic} from "../typechain-types";
-import {AbiCoder, TransactionRequest} from "ethers"
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { IFxBridgeLogic } from "../typechain-types";
+import { AbiCoder, TransactionRequest } from "ethers";
 
 describe("fork network and fx bridge test", function () {
-    let gasAddress: string
-    let bridgeAddress: string
-    let adminAddress: string
-    let bridgeContractName: string
-    const abiCode = new AbiCoder;
+  let gasAddress: string;
+  let bridgeAddress: string;
+  let adminAddress: string;
+  let bridgeContractName: string;
+  const abiCode = new AbiCoder();
 
-    beforeEach(async function () {
-        if (!process.env.FORK_ENABLE) {
-            this.skip()
-        }
-        const network = await ethers.provider.getNetwork()
-        switch (network.chainId.toString()) {
-            case "1":
-                gasAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa"
-                bridgeAddress = "0x6f1D09Fed11115d65E1071CD2109eDb300D80A27"
-                adminAddress = "0x0F413055AdEF9b61e9507928c6856F438d690882"
-                bridgeContractName = "FxBridgeLogicETH"
-                break
-            case "11155111":
-                gasAddress = "0x6Cc9397c3B38739daCbfaA68EaD5F5D77Ba5F455"
-                bridgeAddress = "0xd384a8e8822Ea845e83eb5AA2877239150615C18"
-                adminAddress = "0xcF8049f0B918650614D5bf18CF15af080eFdDEe1"
-                bridgeContractName = "FxBridgeLogicETH"
-                break
-            case "84532":
-                gasAddress = "0x4200000000000000000000000000000000000016"
-                bridgeAddress = "0x9164D153b8Af6D94d41E7876E814DD3Db1AEC320"
-                adminAddress = "0xcF8049f0B918650614D5bf18CF15af080eFdDEe1"
-                bridgeContractName = "FxBridgeLogic"
-                break
-            default:
-                throw new Error("Unsupported network")
-        }
-    })
+  beforeEach(async function () {
+    if (!process.env.FORK_ENABLE) {
+      this.skip();
+    }
+    const network = await ethers.provider.getNetwork();
+    switch (network.chainId.toString()) {
+      case "1":
+        gasAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
+        bridgeAddress = "0x6f1D09Fed11115d65E1071CD2109eDb300D80A27";
+        adminAddress = "0x0F413055AdEF9b61e9507928c6856F438d690882";
+        bridgeContractName = "FxBridgeLogicETH";
+        break;
+      case "11155111":
+        gasAddress = "0x6Cc9397c3B38739daCbfaA68EaD5F5D77Ba5F455";
+        bridgeAddress = "0xd384a8e8822Ea845e83eb5AA2877239150615C18";
+        adminAddress = "0xcF8049f0B918650614D5bf18CF15af080eFdDEe1";
+        bridgeContractName = "FxBridgeLogicETH";
+        break;
+      case "84532":
+        gasAddress = "0x4200000000000000000000000000000000000016";
+        bridgeAddress = "0x9164D153b8Af6D94d41E7876E814DD3Db1AEC320";
+        adminAddress = "0xcF8049f0B918650614D5bf18CF15af080eFdDEe1";
+        bridgeContractName = "FxBridgeLogic";
+        break;
+      default:
+        throw new Error("Unsupported network");
+    }
+  });
 
-    it("upgrade bridge contract", async function () {
-        const gasSigner = await ethers.getImpersonatedSigner(gasAddress)
-        await gasSigner.sendTransaction({
-            to: adminAddress,
-            value: ethers.parseEther("100")
-        })
-        const adminSigner = await ethers.getImpersonatedSigner(adminAddress)
+  it("upgrade bridge contract", async function () {
+    const gasSigner = await ethers.getImpersonatedSigner(gasAddress);
+    await gasSigner.sendTransaction({
+      to: adminAddress,
+      value: ethers.parseEther("100"),
+    });
+    const adminSigner = await ethers.getImpersonatedSigner(adminAddress);
 
-        const bridgeFactory = await ethers.getContractFactory(bridgeContractName)
+    const bridgeFactory = await ethers.getContractFactory(bridgeContractName);
 
-        const bridgeContract = bridgeFactory.attach(bridgeAddress) as IFxBridgeLogic
+    const bridgeContract = bridgeFactory.attach(
+      bridgeAddress
+    ) as IFxBridgeLogic;
 
-        const oldFxBridgeId = await bridgeContract.state_fxBridgeId()
-        const oldPowerThreshold = await bridgeContract.state_powerThreshold()
-        const oldLastEventNonce = await bridgeContract.state_lastEventNonce()
-        const oldCheckpoint = await bridgeContract.state_lastOracleSetCheckpoint()
-        const oldOracleSetNonce = await bridgeContract.state_lastOracleSetNonce()
-        const oldBridgeTokens = await bridgeContract.getBridgeTokenList()
+    const oldFxBridgeId = await bridgeContract.state_fxBridgeId();
+    const oldPowerThreshold = await bridgeContract.state_powerThreshold();
+    const oldLastEventNonce = await bridgeContract.state_lastEventNonce();
+    const oldCheckpoint = await bridgeContract.state_lastOracleSetCheckpoint();
+    const oldOracleSetNonce = await bridgeContract.state_lastOracleSetNonce();
+    const oldBridgeTokens = await bridgeContract.getBridgeTokenList();
 
-        let oldTokenStatus = new Map();
-        for (const bridgeToken of oldBridgeTokens) {
-            const status = await bridgeContract.tokenStatus(bridgeToken.addr)
-            const batchNonce = await bridgeContract.state_lastBatchNonces(bridgeToken.addr)
-            oldTokenStatus.set(bridgeToken.addr.toString(), {batchNonce: batchNonce, status: status})
-        }
+    let oldTokenStatus = new Map();
+    for (const bridgeToken of oldBridgeTokens) {
+      const status = await bridgeContract.tokenStatus(bridgeToken.addr);
+      const batchNonce = await bridgeContract.state_lastBatchNonces(
+        bridgeToken.addr
+      );
+      oldTokenStatus.set(bridgeToken.addr.toString(), {
+        batchNonce: batchNonce,
+        status: status,
+      });
+    }
 
-        const bridgeLogicContract = await bridgeFactory.deploy()
-        await bridgeLogicContract.waitForDeployment()
-        const bridgeLogicContractAddress = await bridgeLogicContract.getAddress()
+    const bridgeLogicContract = await bridgeFactory.deploy();
+    await bridgeLogicContract.waitForDeployment();
+    const bridgeLogicContractAddress = await bridgeLogicContract.getAddress();
 
-        // 0x3659cfe6 is the signature of the upgradeTo(address) function
-        const data = ethers.concat([
-            '0x3659cfe6',
-            abiCode.encode(['address'], [bridgeLogicContractAddress])
-        ])
+    // 0x3659cfe6 is the signature of the upgradeTo(address) function
+    const data = ethers.concat([
+      "0x3659cfe6",
+      abiCode.encode(["address"], [bridgeLogicContractAddress]),
+    ]);
 
-        const transaction: TransactionRequest = {
-            to: bridgeAddress,
-            data: data,
-        }
+    const transaction: TransactionRequest = {
+      to: bridgeAddress,
+      data: data,
+    };
 
-        const upgradeTx = await adminSigner.sendTransaction(transaction)
-        await upgradeTx.wait()
+    const upgradeTx = await adminSigner.sendTransaction(transaction);
+    await upgradeTx.wait();
 
-        const fxBridgeId = await bridgeContract.state_fxBridgeId()
-        const powerThreshold = await bridgeContract.state_powerThreshold()
-        const lastEventNonce = await bridgeContract.state_lastEventNonce()
-        const checkpoint = await bridgeContract.state_lastOracleSetCheckpoint()
-        const oracleSetNonce = await bridgeContract.state_lastOracleSetNonce()
-        const bridgeTokens = await bridgeContract.getBridgeTokenList()
+    const fxBridgeId = await bridgeContract.state_fxBridgeId();
+    const powerThreshold = await bridgeContract.state_powerThreshold();
+    const lastEventNonce = await bridgeContract.state_lastEventNonce();
+    const checkpoint = await bridgeContract.state_lastOracleSetCheckpoint();
+    const oracleSetNonce = await bridgeContract.state_lastOracleSetNonce();
+    const bridgeTokens = await bridgeContract.getBridgeTokenList();
 
-        expect(fxBridgeId).to.equal(oldFxBridgeId)
-        expect(powerThreshold.toString()).to.equal(oldPowerThreshold.toString())
-        expect(lastEventNonce.toString()).to.equal(oldLastEventNonce.toString())
-        expect(checkpoint).to.equal(oldCheckpoint)
-        expect(oracleSetNonce.toString()).to.equal(oldOracleSetNonce.toString())
+    expect(fxBridgeId).to.equal(oldFxBridgeId);
+    expect(powerThreshold.toString()).to.equal(oldPowerThreshold.toString());
+    expect(lastEventNonce.toString()).to.equal(oldLastEventNonce.toString());
+    expect(checkpoint).to.equal(oldCheckpoint);
+    expect(oracleSetNonce.toString()).to.equal(oldOracleSetNonce.toString());
 
-        for (const bridgeToken of bridgeTokens) {
-            const status = await bridgeContract.tokenStatus(bridgeToken.addr)
-            expect(status.isOriginated).to.equal(oldTokenStatus.get(bridgeToken.addr).status.isOriginated)
-            expect(status.isActive).to.equal(oldTokenStatus.get(bridgeToken.addr).status.isActive)
-            expect(status.isExist).to.equal(oldTokenStatus.get(bridgeToken.addr).status.isExist)
-            const batchNonce = await bridgeContract.state_lastBatchNonces(bridgeToken.addr)
-            expect(batchNonce.toString()).to.equal(oldTokenStatus.get(bridgeToken.addr).batchNonce.toString())
-            expect(await bridgeContract.state_lastBridgeCallNonces(1)).to.equal(false)
-        }
-    }).timeout(100000);
+    for (const bridgeToken of bridgeTokens) {
+      const status = await bridgeContract.tokenStatus(bridgeToken.addr);
+      expect(status.isOriginated).to.equal(
+        oldTokenStatus.get(bridgeToken.addr).status.isOriginated
+      );
+      expect(status.isActive).to.equal(
+        oldTokenStatus.get(bridgeToken.addr).status.isActive
+      );
+      expect(status.isExist).to.equal(
+        oldTokenStatus.get(bridgeToken.addr).status.isExist
+      );
+      const batchNonce = await bridgeContract.state_lastBatchNonces(
+        bridgeToken.addr
+      );
+      expect(batchNonce.toString()).to.equal(
+        oldTokenStatus.get(bridgeToken.addr).batchNonce.toString()
+      );
+      expect(await bridgeContract.state_lastBridgeCallNonces(1)).to.equal(
+        false
+      );
+    }
+  }).timeout(100000);
 });
