257b3ec7c4d37cbe22d1f42f4832e729231b8df1
zakir <80246097+zakir-code@users.noreply.github.com>
2024-06-07 12:04:24 +0800

    feat: bridge call support sender call to (#554)
    Co-authored-by: fx0x55 <80245546+fx0x55@users.noreply.github.com>

--- x/crosschain/types/abi.go
+++ x/crosschain/types/abi.go
@@ -8,6 +8,12 @@ import (
 	"github.com/functionx/fx-core/v7/contract"
 )
 
+var MemoSendCallTo = common.HexToHash("0000000000000000000000000000000000000000000000000000000000010000")
+
+func IsMemoSendCallTo(memo []byte) bool {
+	return len(memo) == 32 && common.BytesToHash(memo) == MemoSendCallTo
+}
+
 func PackBridgeCallback(sender common.Address, receiver common.Address, tokens []common.Address, amounts []*big.Int, data, memo []byte) ([]byte, error) {
 	args, err := contract.GetBridgeCallBridgeCallback().Pack("bridgeCallback",
 		sender,
