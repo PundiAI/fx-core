d989047df8c771d67d16bd7258c26ec6c1a7b11b
zakir <80246097+zakir-code@users.noreply.github.com>
2024-07-25 20:28:04 +0800

    feat(crosschain): add oracle_status metrics

--- x/crosschain/types/metrics.go
+++ x/crosschain/types/metrics.go
@@ -4,10 +4,12 @@ const (
 	MetricsKeyBridgeCallIn  = "bridge_call_in"
 	MetricsKeyBridgeCallOut = "bridge_call_out"
 	MetricsKeyOutgoingTx    = "outgoing_tx"
+	MetricsKeyOracleStatus  = "oracle_status"
 )
 
 const (
 	MetricsLabelModule   = "module"
 	MetricsLabelRefund   = "refund"
 	MetricsLabelTxOrigin = "tx_origin"
+	MetricsLabelAddress  = "address"
 )
