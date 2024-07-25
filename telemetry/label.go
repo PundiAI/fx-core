30fcb3a55de1a765a99bfa6a8b886caf7b749ff9
nulnut <151493716+nulnut@users.noreply.github.com>
2024-07-25 18:21:50 +0800

    feat: bridgecall metrics

---
+++ telemetry/label.go
@@ -0,0 +1,9 @@
+package telemetry
+
+const (
+	LabelSender   = "sender"
+	LabelReceiver = "receiver"
+	LabelTo       = "to"
+	LabelSuccess  = "success"
+	LabelToken    = "token"
+)
---
+++ x/crosschain/types/metrics.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	MetricsKeyBridgeCallIn  = "bridge_call_in"
+	MetricsKeyBridgeCallOut = "bridge_call_out"
+	MetricsKeyOutgoingTx    = "outgoing_tx"
+)
+
+const (
+	MetricsLabelModule   = "module"
+	MetricsLabelRefund   = "refund"
+	MetricsLabelTxOrigin = "tx_origin"
+)
