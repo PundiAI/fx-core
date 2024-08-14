4edcd2b82028e1d157732a96da484be0ff8e3871
zakir <80246097+zakir-code@users.noreply.github.com>
2024-08-14 17:12:47 +0800

    fix: migrate community pool spend (#645)
    Co-authored-by: nulnut <151493716+nulnut@users.noreply.github.com>

--- app/upgrades/v7/upgrade.go
+++ app/upgrades/v7/upgrade.go
@@ -1,10 +1,15 @@
 package v7
 
 import (
+	"time"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	autytypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/functionx/fx-core/v7/app/keepers"
@@ -12,12 +17,13 @@ import (
 	fxtypes "github.com/functionx/fx-core/v7/types"
 	crosschaintypes "github.com/functionx/fx-core/v7/x/crosschain/types"
 	fxevmkeeper "github.com/functionx/fx-core/v7/x/evm/keeper"
+	fxgovkeeper "github.com/functionx/fx-core/v7/x/gov/keeper"
 )
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, app *keepers.AppKeepers) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// Testnet skip
-		if fxtypes.ChainId() == fxtypes.TestnetChainId {
+		if ctx.ChainID() == fxtypes.TestnetChainId {
 			return fromVM, nil
 		}
 		// Migrate Tendermint consensus parameters from x/params module to a dedicated x/consensus module.
@@ -37,10 +43,12 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, 
 
 		UpdateWFXLogicCode(cacheCtx, app.EvmKeeper)
 		UpdateFIP20LogicCode(cacheCtx, app.EvmKeeper)
-		crosschainBridgeCallFrom := autytypes.NewModuleAddress(crosschaintypes.ModuleName)
+		crosschainBridgeCallFrom := authtypes.NewModuleAddress(crosschaintypes.ModuleName)
 		if account := app.AccountKeeper.GetAccount(ctx, crosschainBridgeCallFrom); account == nil {
 			app.AccountKeeper.SetAccount(ctx, app.AccountKeeper.NewAccountWithAddress(ctx, crosschainBridgeCallFrom))
 		}
+
+		MigrateCommunityPoolSpendProposals(cacheCtx, app.GovKeeper)
 
 		commit()
 		ctx.Logger().Info("upgrade complete", "module", "upgrade")
@@ -64,4 +72,61 @@ func UpdateFIP20LogicCode(ctx sdk.Context, keeper *fxevmkeeper.Keeper) {
 	} else {
 		ctx.Logger().Info("update FIP20 contract", "module", "upgrade", "codeHash", fip20.CodeHash())
 	}
+}
+
+func MigrateCommunityPoolSpendProposals(ctx sdk.Context, keeper *fxgovkeeper.Keeper) {
+	// migrate inactive CommunityPoolSpendProposal
+	maxEndTime := time.Hour * 24 * 14
+	keeper.IterateInactiveProposalsQueue(ctx, ctx.BlockHeader().Time.Add(maxEndTime), func(proposal v1.Proposal) (stop bool) {
+		ConvertCommunityPoolSpendProposal(ctx, keeper, proposal)
+		return false
+	})
+
+	// migrate active CommunityPoolSpendProposal
+	keeper.IterateActiveProposalsQueue(ctx, ctx.BlockHeader().Time.Add(maxEndTime), func(proposal v1.Proposal) (stop bool) {
+		ConvertCommunityPoolSpendProposal(ctx, keeper, proposal)
+		return false
+	})
+}
+
+func ConvertCommunityPoolSpendProposal(ctx sdk.Context, keeper *fxgovkeeper.Keeper, proposal v1.Proposal) {
+	msgs, err := proposal.GetMsgs()
+	if err != nil {
+		panic(err)
+	}
+	haveSpendMsg := false
+	newMsgs := make([]sdk.Msg, 0, len(msgs))
+	for _, msg := range msgs {
+		legacyMsg, ok := msg.(*v1.MsgExecLegacyContent)
+		if !ok {
+			newMsgs = append(newMsgs, msg)
+			continue
+		}
+		content, err := v1.LegacyContentFromMessage(legacyMsg)
+		if err != nil {
+			panic(err)
+		}
+		cpsp, ok := content.(*distrtypes.CommunityPoolSpendProposal) // nolint:staticcheck
+		if !ok {
+			newMsgs = append(newMsgs, msg)
+			continue
+		}
+		haveSpendMsg = true
+		spendMsg := &distrtypes.MsgCommunityPoolSpend{
+			Authority: keeper.GetAuthority(),
+			Recipient: cpsp.Recipient,
+			Amount:    cpsp.Amount,
+		}
+		newMsgs = append(newMsgs, spendMsg)
+	}
+	if !haveSpendMsg {
+		return
+	}
+	ctx.Logger().Info("migrate community pool spend proposal,", "id", proposal.Id)
+	anyMsgs, err := sdktx.SetMsgs(newMsgs)
+	if err != nil {
+		panic(err)
+	}
+	proposal.Messages = anyMsgs
+	keeper.SetProposal(ctx, proposal)
 }
