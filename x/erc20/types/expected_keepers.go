de1040192b2f2dbd49abbfc98e12211ff91cc640
zakir <80246097+zakir-code@users.noreply.github.com>
2024-09-23 15:36:07 +0800

    chore: remove unused expected interface (#678)

--- x/erc20/types/expected_keepers.go
+++ x/erc20/types/expected_keepers.go
@@ -7,7 +7,6 @@ import (
 	tmbytes "github.com/cometbft/cometbft/libs/bytes"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
@@ -19,25 +18,20 @@ import (
 type AccountKeeper interface {
 	GetModuleAccount(ctx context.Context, moduleName string) sdk.ModuleAccountI
 	GetModuleAddress(moduleName string) sdk.AccAddress
-	GetSequence(ctx context.Context, addr sdk.AccAddress) (uint64, error)
 }
 
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
-	SendCoins(ctx context.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	MintCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
 	BurnCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
 	IsSendEnabledCoin(ctx context.Context, coin sdk.Coin) bool
-	IsSendEnabledCoins(ctx context.Context, coins ...sdk.Coin) error
 	BlockedAddr(addr sdk.AccAddress) bool
 	GetDenomMetaData(ctx context.Context, denom string) (banktypes.Metadata, bool)
 	HasDenomMetaData(ctx context.Context, denom string) bool
 	SetDenomMetaData(ctx context.Context, denomMetaData banktypes.Metadata)
 	GetBalance(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
-	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
-	HasBalance(ctx context.Context, addr sdk.AccAddress, amt sdk.Coin) bool
 }
 
 // EVMKeeper defines the expected EVM keeper interface used on erc20
@@ -52,16 +46,3 @@ type IBCTransferKeeper interface {
 	Transfer(goCtx context.Context, msg *types.MsgTransfer) (*types.MsgTransferResponse, error)
 	GetDenomTrace(ctx sdk.Context, denomTraceHash tmbytes.HexBytes) (types.DenomTrace, bool)
 }
-
-type (
-	ParamSet = paramtypes.ParamSet
-	// Subspace defines an interface that implements the legacy x/params Subspace
-	// type.
-	//
-	// NOTE: This is used solely for migration of x/params managed parameters.
-	Subspace interface {
-		GetParamSet(ctx sdk.Context, ps ParamSet)
-		HasKeyTable() bool
-		WithKeyTable(table paramtypes.KeyTable) paramtypes.Subspace
-	}
-)
