6a7784a8861f83e3290ee025e63d89c27510b66f
zakir <80246097+zakir-code@users.noreply.github.com>
2024-10-18 17:53:36 +0800

    perf: cache origin token amount during crosschain (#755)
    Co-authored-by: nulnut <151493716+nulnut@users.noreply.github.com>

--- x/crosschain/keeper/bridge_call_out.go
+++ x/crosschain/keeper/bridge_call_out.go
@@ -247,6 +247,8 @@ func (k Keeper) BridgeCallBaseCoin(
 	fxTarget *types.FxTarget,
 	originTokenAmount sdkmath.Int,
 ) (uint64, error) {
+	var cacheKey string
+	var nonce uint64
 	if fxTarget.IsIBC() {
 		if !coins.IsValid() || len(coins) != 1 {
 			return 0, sdkerrors.ErrInvalidCoins.Wrapf("ibc transfer with coins: %s", coins.String())
@@ -260,20 +262,25 @@ func (k Keeper) BridgeCallBaseCoin(
 		if err != nil {
 			return 0, err
 		}
-		sequence, err := k.IBCTransfer(ctx, from.Bytes(), toAddr, ibcCoin, fxTarget.IBCChannel, string(memo))
+		nonce, err = k.IBCTransfer(ctx, from.Bytes(), toAddr, ibcCoin, fxTarget.IBCChannel, string(memo))
 		if err != nil {
 			return 0, err
 		}
-		if originTokenAmount.IsPositive() {
-			ibcTransferKey := types.NewIBCTransferKey(fxTarget.IBCChannel, sequence)
-			if err = k.erc20Keeper.SetCache(ctx, ibcTransferKey); err != nil {
-				return 0, err
-			}
+		cacheKey = types.NewIBCTransferKey(fxTarget.IBCChannel, nonce)
+	} else {
+		var err error
+		if nonce, err = k.AddOutgoingBridgeCall(ctx, from, refund, coins, to, data, memo, 0); err != nil {
+			return 0, err
 		}
-		return sequence, nil
+		cacheKey = types.NewOriginTokenKey(k.moduleName, nonce)
 	}
-	// todo record origin amount
-	return k.AddOutgoingBridgeCall(ctx, from, refund, coins, to, data, memo, 0)
+
+	if originTokenAmount.IsPositive() {
+		if err := k.erc20Keeper.SetCache(ctx, cacheKey, originTokenAmount); err != nil {
+			return 0, err
+		}
+	}
+	return nonce, nil
 }
 
 func (k Keeper) CrosschainBaseCoin(
@@ -301,7 +308,7 @@ func (k Keeper) CrosschainBaseCoin(
 	}
 
 	if originToken {
-		return k.erc20Keeper.SetCache(ctx, cacheKey)
+		return k.erc20Keeper.SetCache(ctx, cacheKey, amount.Amount.Add(fee.Amount))
 	}
 	return nil
 }
