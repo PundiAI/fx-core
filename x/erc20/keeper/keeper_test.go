a8bc0bfb2bd3279ad8e91181eadd1727e1ad6c45
nulnut <151493716+nulnut@users.noreply.github.com>
2025-02-12 21:03:30 +0800

    test: add tests for erc20 module

--- x/erc20/keeper/keeper_test.go
+++ x/erc20/keeper/keeper_test.go
@@ -6,25 +6,31 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/pundiai/fx-core/v8/testutil/helpers"
-	erc20keeper "github.com/pundiai/fx-core/v8/x/erc20/keeper"
+	"github.com/pundiai/fx-core/v8/x/erc20/keeper"
 )
 
 type KeeperTestSuite struct {
 	helpers.BaseSuite
+	erc20TokenSuite helpers.ERC20TokenSuite
+
+	signer *helpers.Signer
 }
 
 func TestERC20KeeperTestSuite(t *testing.T) {
-	suite.Run(t, &KeeperTestSuite{})
+	suite.Run(t, new(KeeperTestSuite))
 }
 
 func (suite *KeeperTestSuite) SetupTest() {
 	suite.BaseSuite.SetupTest()
+	suite.erc20TokenSuite = helpers.NewERC20Suite(suite.Require(), suite.App.EvmKeeper)
+
+	suite.signer = suite.AddTestSigner()
 }
 
 func (suite *KeeperTestSuite) SetupSubTest() {
 	suite.SetupTest()
 }
 
-func (suite *KeeperTestSuite) GetKeeper() erc20keeper.Keeper {
+func (suite *KeeperTestSuite) GetKeeper() keeper.Keeper {
 	return suite.App.Erc20Keeper
 }
