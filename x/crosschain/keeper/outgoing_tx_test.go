90ac81f977681b0427e3334a34f2761678f0ea3f
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2025-02-12 15:28:12 +0800

    refactor(x/crosschain): reset the block height when ResendTimeoutOutgoingTxBatch

--- x/crosschain/keeper/outgoing_tx_test.go
+++ x/crosschain/keeper/outgoing_tx_test.go
@@ -1,20 +1,17 @@
 package keeper_test
 
 import (
+	sdkmath "cosmossdk.io/math"
+	tmrand "github.com/cometbft/cometbft/libs/rand"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/pundiai/fx-core/v8/testutil/helpers"
 	"github.com/pundiai/fx-core/v8/x/crosschain/types"
 )
 
 func (suite *KeeperTestSuite) TestKeeper_IterateOutgoingTxBatches() {
-	tokenContract := helpers.GenExternalAddr(suite.chainName)
 	batchNumber := 3
-	for i := 1; i <= batchNumber; i++ {
-		err := suite.Keeper().StoreBatch(suite.Ctx, &types.OutgoingTxBatch{
-			BatchNonce:    uint64(i),
-			TokenContract: tokenContract,
-		})
-		suite.NoError(err)
-	}
+	suite.addOutgoingTxBatchs(batchNumber, helpers.GenExternalAddr(suite.chainName))
 	batchList := make([]*types.OutgoingTxBatch, 0)
 	suite.Keeper().IterateOutgoingTxBatches(suite.Ctx, func(batch *types.OutgoingTxBatch) bool {
 		batchList = append(batchList, batch)
@@ -22,5 +19,58 @@ func (suite *KeeperTestSuite) TestKeeper_IterateOutgoingTxBatches() {
 	})
 	for i := 0; i < batchNumber; i++ {
 		suite.Equal(batchList[i].BatchNonce, uint64(i+1))
+	}
+}
+
+func (suite *KeeperTestSuite) TestResendTimeoutOutgoingTxBatch() {
+	tokenContract := helpers.GenExternalAddr(suite.chainName)
+	suite.addOutgoingTxBatchs(5, tokenContract)
+	storeKey := suite.App.GetKVStoreKey()[suite.Keeper().ModuleName()]
+	// set last batch id to 6
+	suite.Ctx.KVStore(storeKey).Set(types.KeyLastOutgoingBatchID, sdk.Uint64ToBigEndian(6))
+
+	bz := suite.Ctx.KVStore(storeKey).Get(types.KeyLastOutgoingBatchID)
+	suite.EqualValues(6, sdk.BigEndianToUint64(bz))
+
+	suite.Commit(3)
+	// if batchNonce 5 is executed, 1,2,3,4 will be resend
+	err := suite.Keeper().OutgoingTxBatchExecuted(suite.Ctx, tokenContract, 5)
+	suite.NoError(err)
+
+	actualBatchList := make([]uint64, 0, 4)
+	suite.Keeper().IterateOutgoingTxBatches(suite.Ctx, func(batch *types.OutgoingTxBatch) bool {
+		suite.EqualValues(uint64(suite.Ctx.BlockHeight()), batch.Block)
+		actualBatchList = append(actualBatchList, batch.BatchNonce)
+		return false
+	})
+	// expected batch 6,7,8,9
+	suite.EqualValues([]uint64{6, 7, 8, 9}, actualBatchList)
+}
+
+func (suite *KeeperTestSuite) addOutgoingTxBatchs(batchNumber int, tokenContract string) {
+	for i := 1; i <= batchNumber; i++ {
+		err := suite.Keeper().StoreBatch(suite.Ctx, &types.OutgoingTxBatch{
+			// save batch with same block height
+
+			Block:         1,
+			BatchNonce:    uint64(i),
+			TokenContract: tokenContract,
+			Transactions: []*types.OutgoingTransferTx{
+				{
+					Id:          1,
+					Sender:      helpers.GenAccAddress().String(),
+					DestAddress: helpers.GenExternalAddr(suite.chainName),
+					Token: types.ERC20Token{
+						Contract: tokenContract,
+						Amount:   sdkmath.NewInt(tmrand.Int63()),
+					},
+					Fee: types.ERC20Token{
+						Contract: tokenContract,
+						Amount:   sdkmath.NewInt(tmrand.Int63()),
+					},
+				},
+			},
+		})
+		suite.NoError(err)
 	}
 }
