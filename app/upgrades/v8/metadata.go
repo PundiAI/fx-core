09565ebd76ca6dfc13bb71b9596883828576f0e5
zakir <80246097+zakir-code@users.noreply.github.com>
2025-03-10 17:20:08 +0800

    test: add migrate metadata unit test (#1060)

--- app/upgrades/v8/metadata.go
+++ app/upgrades/v8/metadata.go
@@ -11,7 +11,8 @@ func updateMetadataDesc(ctx sdk.Context, bankKeeper bankkeeper.Keeper) {
 		if md.Description == "The cross chain token of the Function X" ||
 			md.Description == "Cross chain token of Function X" ||
 			md.Description == "The cross chain token of Function X" ||
-			md.Description == "Cross chain token Function X" {
+			md.Description == "Cross chain token Function X" ||
+			md.Description == "Function X coin token representation of 0x934B9f502dcED1eBf0594c7384Eb299bC3ca2bE6" {
 			md.Description = "The crosschain token of the Pundi AIFX"
 		}
 		bankKeeper.SetDenomMetaData(ctx, md)
