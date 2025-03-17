6470bdf53e61133959648209022cc3b4ae15887f
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2025-03-17 08:11:04 +0000

    fix: prevent global error from being overwritten

--- server/start.go
+++ server/start.go
@@ -299,7 +299,7 @@ func startInProcess(svrCtx *server.Context, clientCtx client.Context, opts ether
 
 	app := opts.AppCreator(svrCtx.Logger, db, traceWriter, svrCtx.Viper)
 	defer func() {
-		if err = app.Close(); err != nil {
+		if closeErr := app.Close(); closeErr != nil {
 			logger.Error("close application failed", "error", err.Error())
 		}
 	}()
